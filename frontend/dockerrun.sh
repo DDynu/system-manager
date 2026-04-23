8b8dbdb060b84b657939ca1093449e89c8790810
DDynu <khoadang826@gmail.com>
2026-04-23 21:22:47 +0700

    Added line to cd to script before build Dockerfile

--- frontend/dockerrun.sh
+++ frontend/dockerrun.sh
@@ -1,3 +1,6 @@
+#!//bin/bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR
 docker build -t system-manager .
 docker rm -f system-manager 2>&1
 docker run --name system-manager -d -p 8085:80 system-manager:latest
