7600b8c308654aee78039bdabfe3e1e178d850fc
DDynu <khoadang826@gmail.com>
2026-04-23 11:21:15 +0700

    Fix power start script for correct path, then update requirements.txt

--- backend/power/start.sh
+++ backend/power/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/suponer/Documents/Codes/AICodes/system-manager/backend/power/
+cd /home/suponer/Documents/Codes/system-manager/backend/power/
 source ../venv/bin/activate
-export PYTHONPATH=/home/suponer/Documents/Codes/AICodes/system-manager:$PYTHONPATH
+export PYTHONPATH=/home/suponer/Documents/Codes/system-manager:$PYTHONPATH
 uvicorn app:app --reload --host 0.0.0.0 --port 8001
